faeda928a45e47c4e75c3f2266a8a32ac56875ba
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-24 20:44:42 +0545

    mdx added

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,50 +1,50 @@
-import { makeSource,defineDocumentType } from 'contentlayer-source-notion'
+import { makeSource, defineDocumentType } from "contentlayer-source-notion";
 
 const Doc = defineDocumentType(() => ({
-  name: 'Blog',
-  filePathPattern: '**/**/*.md',
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
   fields: {
     title: {
-      type: 'string',
+      type: "string",
     },
-    publishAt:{
-      type: 'date',
+    publishedAt: {
+      type: "date",
       required: true,
     },
-     updatedAt:{
-      type: 'date',
+    updatedAt: {
+      type: "date",
       required: true,
     },
-     description:{
-      type: 'string',
+    description: {
+      type: "string",
       required: true,
     },
     image: {
-      type: 'string',
-      required: true
-    },
-    isPublished: {
-      type: 'boolean',
-    },
-     author:{
-      type: 'string',
+      type: "string",
       required: true,
     },
-    tags:{
-      type: 'list',
-      of: {type: 'string'},
-    }
+    isPublished: {
+      type: "boolean",
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
   },
-   computedFields: {
+  computedFields: {
     url: {
-      type: 'string',
+      type: "string",
       resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
     },
   },
-}))
- 
+}));
+
 export default makeSource({
   /* options */
-  contentDirPath: 'content',
-  DocumentType: [Blog]
-})
+  contentDirPath: "content",
+  DocumentType: [Blog],
+});
